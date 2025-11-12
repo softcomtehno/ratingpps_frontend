235c3d32118958546890e87dd049726199b0f3ad
quoazh <zhukovilia52@gmail.com>
2025-11-12 09:06:20 +0600

    Открыто заполнение

--- src/components/RegNav.jsx
+++ src/components/RegNav.jsx
@@ -5,10 +5,10 @@ const RegNav = () => {
     <>
       <div className="header__menu-s">
         <Link to="/private_office" className="head__item_1 Montherat">Личные данные</Link>
-        {/* <Link to="/Progress" className="head__item Montherat">Личные достижения</Link>
+        <Link to="/Progress" className="head__item Montherat">Личные достижения</Link>
         <Link to="/Ural" className="head__item Montherat">Научно-исследовательская деятельность</Link>
         <Link to="/Education" className="head__item Montherat">Инновационно-образовательная деятельность</Link>
-        <Link to="/Social" className="head__item Montherat">Воспитательная, общественная деятельность</Link> */}
+        <Link to="/Social" className="head__item Montherat">Воспитательная, общественная деятельность</Link>
       </div>
     </>
   );
